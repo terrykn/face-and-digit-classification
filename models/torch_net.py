ce5a2805998fddd25b4bd5255007918fc2f6ec6a
Terry Nguyen <terryknguyen@gmail.com>
2025-05-02 10:25:47 -0400

    add batch normalization to torchnet

--- models/torch_net.py
+++ models/torch_net.py
@@ -5,11 +5,13 @@ class TorchNet(nn.Module):
     def __init__(self, input_dim, hidden1=128, hidden2=64, output_dim=10):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, hidden1)
+        self.bn1 = nn.BatchNorm1d(hidden1)  # batch normalization 
         self.fc2 = nn.Linear(hidden1, hidden2)
+        self.bn2 = nn.BatchNorm1d(hidden2) 
         self.fc3 = nn.Linear(hidden2, output_dim)
         self.relu = nn.ReLU()
         
     def forward(self, x):
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        x = self.relu(self.bn1(self.fc1(x))) # batch normalization
+        x = self.relu(self.bn2(self.fc2(x)))
         return self.fc3(x)
